b352ecb180f8315865cc4b6454c3f7788021aff7
Sanket Rathod <sanketrathod7420@gmail.com>
2024-10-16 19:06:51 +0530

    error local host updated

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL, 'https://chatapp-client-rho.vercel.app', 'http://localhost:3000'],
     credentials: true
 }))
 app.use(express.json())
