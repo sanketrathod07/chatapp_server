b66d1ea86cd6b3556f141e0651481a115b3a8b82
Sanket Rathod <sanketrathod7420@gmail.com>
2024-10-16 18:56:24 +0530

    local host changes

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const { app, server } = require('./socket/index')
 
 // const app = express()
 app.use(cors({
-    origin: [process.env.FRONTEND_URL, 'http://192.168.31.49:3000'],
+    origin: process.env.FRONTEND_URL,
     credentials: true
 }))
 app.use(express.json())
